508741deacb289399771d766775cfe669334c9cf
Keshari07 <kbs882763gmail.com>
2026-02-08 06:51:49 +0530

    Data Of 08 Feb 2026

---
+++ Simple Code Python/Add.py
@@ -0,0 +1,64 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+# Addition
+add = num1 + num2
+print("Addition =", add)
+
+
+
+______________________________________________________________________________________________________________________________________________________________________________________
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ Date = 14/01/2026
+
+# """
+# my_string1 = 'hello world'
+# print(my_string1)
+
+# my_string2 = "I am Aadarsh"
+# print(my_string2)
+
+
+
+# my_string3 = """i am aadarsh """
+# print(my_string3)
+# """
+
+my_set = {1,2,3,4}
+print(my_set)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ============================================================================== Date = 15/01/2026 
+
+# # A = int(input("Enter The Any Number : "))
+# # print(A)
+# # A = int(input("Enter any number: "))
+# # A = int(23.5)
+# # A = float(25344)
+# # print(A)
+# num4 = complex('3+5j')
+# print(num4)  # prints (3 + 5j)
+
+
+
+student = {
+    "Name" : "Suryavanshi",
+    "Age": 26,
+     "City":"Sagar",
+     "College":"AIST"
+}
+print(student)
